188277f29063e4111387c402a945c2ffd3a43be0
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-03-16 08:30:10 +0300

    Добавлен Router.php

---
+++ components/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+class Router
+{
+    private $routses;
+
+    public function __construct()
+    {
+
+    }
+
+    public function run()
+    {
+        echo 'Class Router method run';
+    }
+}
