d325543593dacdb9e2db12bdb4e994d7ba71f984
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-03-16 09:13:11 +0300

    Получил строку запроса в Router.php;

--- components/Router.php
+++ components/Router.php
@@ -10,9 +10,31 @@ class Router
         $this->routes = include($routesPath);
     }
 
+    /**
+     * Returns request string
+     */
+
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
+
     public function run()
     {
-        print_r($this->routes);
-        echo 'Class Router method run';
+        // Получить строку запроса
+        $uri = $this->getURI();
+        echo $uri;
+
+        // Проверить наличие такого запроса в routes.php
+
+        // Если есть совпадение,определить какой контроллер
+        // и action обрабатывают запрос
+
+        // Подключить файл класса контроллера
+
+        // Создать объект, вызвать метод(т.е action)
+
     }
 }
